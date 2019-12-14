8608ccda1fafcd355da71765218d043ca219cc2a
suxing9980 <suxing9980@163.com>
2019-12-14 15:29:20 +0800

    one line

---
+++ chapter01/2_不换行输出.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#@Time    : 2019/12/14 14:56
+#@Author  : Little柯南
+#@File    : 2_不换行输出.py
+
+a = 'hello'
+b = ' world'
+print(a, end=" ")
+print(b, end=' ')
