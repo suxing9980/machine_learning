e25751ad1e9ca6f497a99ceb55bbf1736e18ae77
suxing9980 <suxing9980@163.com>
2019-12-14 15:29:35 +0800

    import

---
+++ chapter01/3_导入模块.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#@Time    : 2019/12/14 15:06
+#@Author  : Little柯南
+#@File    : 3_导入模块.py
+
+'''
+1、反斜杠可以用来转义，使用r可以让反斜杠不发生转义。
+2、字符串可以用+运算符连接在一起，用*运算符重复。
+3、Python中的字符串有两种索引方式，从左往右以0开始，从右往左以-1开始。
+4、Python中的字符串不能改变。
+'''
+
+import sys
+print('================Python import mode==========================')
+print ('命令行参数为:')
+for i in sys.argv:
+    print (i)
+print ('\n python 路径为',sys.path)
