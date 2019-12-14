76133a07b0aa8b31dbf47d1159996c183bc434be
suxing9980 <suxing9980@163.com>
2019-12-14 14:50:45 +0800

    str test

--- chapter01/1_first.py
+++ chapter01/1_first.py
@@ -5,4 +5,20 @@
 #@Author  : Little柯南
 #@File    : 1_first.py
 
+import keyword
+
 print("Hello World")
+print(keyword.kwlist)
+
+str = 'Runoob'
+print(str)
+print(str[0:-1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + '你好')# + 紧接着跟上后面的
+print(str,'你好')  # ，空格后再跟上后面的
+print('**************************')
+print('hello\nrunoob')
+print(r'hello\nrunoob')
