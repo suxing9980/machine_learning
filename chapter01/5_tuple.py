3442374a046fc019d1d6fd16fc5a42391a4e6c64
suxing9980 <suxing9980@163.com>
2019-12-15 22:42:09 +0800

    tuple

---
+++ chapter01/5_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#@Time    : 2019/12/15 22:01
+#@Author  : Little柯南
+#@File    : 5_tuple.py
+
+tup1 = ('Google', 'Runoob', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5, 6, 7)
+tup3 = "a", "b", "c", "d"  #  不需要括号也可以
+print(type(tup3))
+
+print("tup1[0]: ", tup1[0])
+print("tup2[1:5]: ", tup2[1:5])
