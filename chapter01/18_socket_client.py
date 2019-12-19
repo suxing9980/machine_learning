08bcbefa18eebaeaaa83f87b94e669483d33bf9a
suxing9980 <suxing9980@163.com>
2019-12-19 18:28:15 +0800

    socket client

---
+++ chapter01/18_socket_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @Time    : 2019/12/19 17:59
+# @Author  : Little柯南
+# @File    : 18_socket_client.py
+
+import socket
+import sys
+
+# 创建socket对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取本地主机名
+host = socket.gethostname()
+print('host:', host)
+
+# 设置端口号
+port = 9999
+# 连接服务，指定主机和端口(host, port)必须是元组,like bind
+s.connect((host, port))
+
+# 接收小于1024字节的数据
+msg = s.recv(1024)
+print(msg)
+
+s.close()
